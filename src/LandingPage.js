d3ca5b95d99b2b04b43013465a4f9ec5644c2546
subhasis-chand <60152943+subhasis-chand@users.noreply.github.com>
2024-02-07 07:00:31 +0000

    Adding card

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,58 @@
 import Header from "./Header";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
 
 const LandingPage = () => {
-  return(
-    <div style={{width: '100%'}}>
+  return (
+    <div style={{ width: '100%', padding: '10px', boxSizing: 'border-box' }}>
       <Header />
-      Landing page
+      <Card style={{ backgroundColor: 'coral', color: 'darkviolet' }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          // action={
+          //   <IconButton aria-label="settings">
+          //     <MoreVertIcon />
+          //   </IconButton>
+          // }
+          title="Sri Somnath Temple Live Darshan"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="/static/images/cards/paella.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+
+      </Card>
     </div>
   );
 }

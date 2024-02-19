ce84fba6ff5328905d0fa1464edcd2058070f027
subhasis-chand <60152943+subhasis-chand@users.noreply.github.com>
2024-02-19 13:14:13 +0000

    og changes

--- src/CardWithVideo.js
+++ src/CardWithVideo.js
@@ -1,66 +1,103 @@
-import Card from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import ShareIcon from '@mui/icons-material/Share';
+import Card from '@mui/material/Card';
 import ReactPlayer from "react-player/lazy";
+import { getDateToday } from './Helper';
+import { useState } from 'react';
+import { Collapse, Typography } from '@mui/material';
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const CardWithVideo = ({
-	iconImage,
-	title,
-	subheader,
-	videoURL,
-	description
+  title,
+  videoURL,
 }) => {
-	return(
-		<Card style={{ color: '#3e2200', marginBottom: '20px' }}>
-        <CardHeader
-          avatar={
-            <div style={{
-              height: '60px',
-              width: '60px',
-              borderRadius: '30px',
-              boxSizing: 'border-box',
-              border: '1px solid coral',
-              overflow: 'hidden'
-            }}>
-              <img
-                src={iconImage}
-								alt='icon'
-                style={{
-                  height: '100%',
-                  width: '100%',
-                  objectFit: 'contain'
-                }}
-              />
-            </div>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <ShareIcon />
-            </IconButton>
-          }
-          title={title}
-          subheader={subheader}
-        />
-        <ReactPlayer
-          url={videoURL}
-          loop={true}
-          controls={true}
-          // height={"100%"}
-          width={"100%"}
-          playbackRate={1}
-          muted={false}
-        />
+  const [expanded, setExpanded] = useState(false);
 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card style={{ color: '#3e2200', marginBottom: '30px' }}>
+      <ReactPlayer
+        url={videoURL}
+        loop={true}
+        controls={true}
+        width={"100%"}
+        playbackRate={1}
+        muted={false}
+      // light={SomnathTemple}
+      />
+
+      <CardContent
+        style={{ padding: '0' }}
+      >
+        <CardHeader
+          titleTypographyProps={{ variant: 'h6' }}
+          title={title}
+          subheader={getDateToday()}
+        />
+      </CardContent>
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandCircleDownIcon fontSize='large' />
+        </ExpandMore>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            {description}
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+            stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don&apos;t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
           </Typography>
         </CardContent>
-      </Card>
-	);
+      </Collapse>
+    </Card>
+  );
 }
 
 export default CardWithVideo;
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,17 +1,25 @@
 import Header from "./Header";
-import SomnathTempleIcon from './Assets/somnathtemple.jpeg'
 import CardWithVideo from "./CardWithVideo";
 
 const LandingPage = () => {
   return (
     <div style={{ width: '100%', padding: '5px', boxSizing: 'border-box' }}>
       <Header />
-      <CardWithVideo 
-      iconImage={SomnathTempleIcon}
-      title="Sri Somnath Temple Live Darshan"
-      subheader="September 14, 2016"
-      videoURL="https://www.youtube.com/embed/h4cYkgPnYQk"
-      description='Somnath Temple, located in Gujarat, India, is one of the oldest and most revered temples dedicated to Lord Shiva. Its history dates back to antiquity, with references found in ancient texts like the Puranas. Despite facing numerous invasions and destructions over the centuries, the temple has been rebuilt multiple times and continues to stand as a symbol of resilience and devotion.'
+      <CardWithVideo
+        title="Shree Siddhivinayak Mandir Live Darshan"
+        videoURL="https://www.youtube.com/embed/live_stream?channel=UCNH47WOVuA2zkP2cP1sEynw"
+      />
+      <CardWithVideo
+        title="Shree Somnath Mandir Live Darshan"
+        videoURL="https://www.youtube.com/watch?v=_3mhms1D78g"
+      />
+      <CardWithVideo
+        title="Shree Shirdi Sai Baba Mandir Live Darshan"
+        videoURL="https://www.youtube.com/embed/live_stream?channel=UCgbNbIiypftakb4bsAAhTVw"
+      />
+      <CardWithVideo
+        title="Shree Dwarakadhish Jee Live Darshan"
+        videoURL="https://www.youtube.com/embed/live_stream?channel=UCBAvMHZO3BIfMMhOK9LMOYQ"
       />
     </div>
   );
